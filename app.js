ce98e75d75f24ec4905e203d8f37b5aea10bba07
Fahima-anny <fahima.info03@gmail.com>
2024-03-01 16:39:44 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,135 @@
+let displayBar = document.getElementById("displayBar")
+displayBar.style.height = "80px"
+displayBar.style.paddingTop = "15px"
+displayBar.style.display = "flex"
+
+function clearF(){
+    displayBar.innerText= ""
+}
+
+function offF(){
+    displayBar.innerText= ""
+    displayBar.style.backgroundColor = "darkGray"
+}
+
+function onF(){
+    displayBar.style.backgroundColor = "#8DABC5"
+    displayBar.innerText= ""
+}
+
+function btn7F(){
+  let add = document.createElement("p")
+  add.innerText = btn7.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn8F(){
+  let add = document.createElement("p")
+  add.innerText = btn8.innerText 
+  displayBar.appendChild(add)
+}
+
+function plusBtnF(){
+  let add = document.createElement("p")
+  add.innerText =plusBtn.innerText 
+  displayBar.appendChild(add)
+}
+
+function minusBtnF(){
+  let add = document.createElement("p")
+  add.innerText =minusBtn.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn9F(){
+  let add = document.createElement("p")
+  add.innerText =btn9.innerText 
+  displayBar.appendChild(add)
+}
+
+function multiBtnF(){
+  let add = document.createElement("p")
+  add.innerText =multiBtn.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn4F(){
+  let add = document.createElement("p")
+  add.innerText =btn4.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn5F(){
+  let add = document.createElement("p")
+  add.innerText =btn5.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn6F(){
+  let add = document.createElement("p")
+  add.innerText =btn6.innerText 
+  displayBar.appendChild(add)
+}
+
+function divisionBtnF(){
+  let add = document.createElement("p")
+  add.innerText =divisionBtn.innerText 
+  displayBar.appendChild(add)
+}
+
+function Btn1F(){
+  let add = document.createElement("p")
+  add.innerText =btn1.innerText 
+  displayBar.appendChild(add)
+}
+
+function Btn2F(){
+  let add = document.createElement("p")
+  add.innerText =btn2.innerText 
+  displayBar.appendChild(add)
+}
+
+function Btn3F(){
+  let add = document.createElement("p")
+  add.innerText =btn3.innerText 
+  displayBar.appendChild(add)
+}
+
+function percentageBtnF(){
+  let add = document.createElement("p")
+  add.innerText =percentageBtn.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn0F(){
+  let add = document.createElement("p")
+  add.innerText =btn0.innerText 
+  displayBar.appendChild(add)
+}
+
+function btn00F(){
+  let add = document.createElement("p")
+  add.innerText =btn00.innerText 
+  displayBar.appendChild(add)
+}
+
+function toThePowerBtnF(){
+  let add = document.createElement("p")
+  add.innerText =toThePowerBtn.innerText 
+  displayBar.appendChild(add)
+}
+
+
+
+
+
+
+
+
+
+
+    
+ 
+
+
+
